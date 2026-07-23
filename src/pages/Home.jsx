2205fb5bfc4674b2df4d3b9ed491bbb53925eac8
coder-utshab <ghoshutshob470@gmail.com>
2026-07-23 22:05:03 +0530

    up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,29 @@
-import Banner from "../components/Banner/Banner";
-import Books from "../components/Books/Books";
-import Footer from "../components/Footer/Footer";
+// import Banner from "../components/Banner/Banner";
+// import Books from "../components/Books/Books";
+// import Footer from "../components/Footer/Footer";
+
+// const Home = () => {
+//   return (
+//     <>
+//       <Banner />
+//       <Books />
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Home;
+
+
+
 
 const Home = () => {
   return (
-    <>
-      <Banner />
-      <Books />
-      <Footer />
-    </>
+    <div className="p-10">
+      <h1 className="text-5xl font-bold text-red-500">
+        Tailwind Working
+      </h1>
+    </div>
   );
 };
 
