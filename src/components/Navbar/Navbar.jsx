995ecf1e1ef1ea0787e287041b0f2be9c869cd47
coder-utshab <ghoshutshob470@gmail.com>
2026-07-23 22:10:38 +0530

    up

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,44 @@
 import { NavLink } from "react-router";
 
 const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/listed-books">Listed Books</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/pages-to-read">Pages to Read</NavLink>
+      </li>
+    </>
+  );
+
   return (
-    <nav>
-      <h2>Book Vibe</h2>
-
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-
-        <li>
-          <NavLink to="/listed-books">Listed Books</NavLink>
-        </li>
-
-        <li>
-          <NavLink to="/pages-to-read">Pages to Read</NavLink>
-        </li>
-      </ul>
-
-      <div>
-        <button>Sign In</button>
-        <button>Sign Up</button>
+    <div className="navbar bg-base-100 py-5">
+      <div className="navbar-start">
+        <a className="text-3xl font-bold">Book Vibe</a>
       </div>
-    </nav>
+
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 font-semibold">
+          {links}
+        </ul>
+      </div>
+
+      <div className="navbar-end gap-3">
+        <button className="btn bg-[#23BE0A] text-white">
+          Sign In
+        </button>
+
+        <button className="btn bg-[#59C6D2] text-white">
+          Sign Up
+        </button>
+      </div>
+    </div>
   );
 };
 
